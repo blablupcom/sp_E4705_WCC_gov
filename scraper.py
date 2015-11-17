d1e84b585eac349f15456336a05837c0d3d10000
blablupcom <o.volodin@yahoo.com>
2015-11-17 14:18:18 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,8 +12,7 @@ from bs4 import BeautifulSoup
 
 #### FUNCTIONS 1.2
 
-import requests       # import requests to validate URL
-import urllib
+import urllib         # import requests to validate URL
 
 def validateFilename(filename):
     filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
@@ -40,29 +39,7 @@ def validateFilename(filename):
 
 
 def validateURL(url):
-    # try:
-    #     r = requests.get(url, allow_redirects=True, timeout=20)
-    #     count = 1
-    #     while r.status_code == 500 and count < 4:
-    #         print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
-    #         count += 1
-    #         r = requests.get(url, allow_redirects=True, timeout=20)
-    #     sourceFilename = r.headers.get('Content-Disposition')
-    #     print sourceFilename
-    #     if sourceFilename:
-    #         ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
-    #     elif r.headers['Content-Type'] == 'text/csv':
-    #         ext = '.csv'
-    #     else:
-    #         ext = os.path.splitext(url)[1]
-    #     print ext
-    #     validURL = r.status_code == 200
-    #     validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
-    #     return validURL, validFiletype
-    # # except:
-    #     print ("Error validating URL.")
-    #     return False, False
-    # try:
+    try:
         r = urllib.urlopen(url)
         count = 1
         while r.getcode() == 500 and count < 4:
@@ -79,7 +56,7 @@ def validateURL(url):
         validURL = r.getcode() == 200
         validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
-    # except:
+    except:
         print ("Error validating URL.")
         return False, False
 
